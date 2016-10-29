4ce394897c4c43b795769bcf4728966dbf5f6d60
Alexander <mralexsaavedra@gmail.com>
2016-10-29 13:03:35 +0200

    BildumakTableUI

--- FlickrBackup/src/probak/MyInternalFrame.java
+++ FlickrBackup/src/probak/MyInternalFrame.java
@@ -8,7 +8,7 @@ public class MyInternalFrame extends JInternalFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	static int openFrameCount = 0;
-    static final int xOffset = 50, yOffset = 50;
+    static final int xOffset = 30, yOffset = 30;
 
     public MyInternalFrame() {
         super("Document #" + (++openFrameCount), 
