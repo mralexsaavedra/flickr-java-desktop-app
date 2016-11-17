129aaa292f3b2774589fd497994093040bd867ee
Alexander <mralexsaavedra@gmail.com>
2016-11-17 12:21:33 +0100

    .gitignore-n datu basea gehitu

--- FlickrBackup/src/kudeatzaileak/Kudeatzailea.java
+++ FlickrBackup/src/kudeatzaileak/Kudeatzailea.java
@@ -1,11 +1,5 @@
 package kudeatzaileak;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-
 public class Kudeatzailea {
 
 	private static final Kudeatzailea kudeatzaile = new Kudeatzailea();
