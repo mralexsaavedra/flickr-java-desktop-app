e9fe2a3b631c0313bdc04d871ef225ab79687290
Alexander <mralexsaavedra@gmail.com>
2016-12-20 21:59:06 +0100

    Ia badago amaituta

--- FlickrBackup/src/sesioPantailak_UI/HasiPantaila_UI.java
+++ FlickrBackup/src/sesioPantailak_UI/HasiPantaila_UI.java
@@ -8,15 +8,17 @@ import flickrAuth.Auth_UI;
 public class HasiPantaila_UI{
 
 	public HasiPantaila_UI(){
-		int erantzuna = JOptionPane.showConfirmDialog(null, "setup.properties eginda daukazu?", "Hasi baino lehen...", JOptionPane.YES_NO_OPTION);
+		int erantzuna = JOptionPane.showConfirmDialog(null, "Hasi baino lehen... setup.properties eginda daukazu?");
 		if (erantzuna==0){
 			SesioaHasiPantaila sesioa = new SesioaHasiPantaila();
 			sesioa.panelaEraikitzen();
 		}
-		else{
+		else if (erantzuna==1){
 			Auth_UI auth = new Auth_UI();
 			auth.eraiki();
 		}
+		else
+			System.exit(0);
 	}
 	
 	public static void main(String[] args) {
